875328d40aa50f6cea534211b5de2ed98a09120c
davoodwadi <62418145+davoodwadi@users.noreply.github.com>
2025-10-20 11:42:52 -0700

    update

--- src/page_analysis.py
+++ src/page_analysis.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-from .analysis_component import analysis_component
+from .analysis_component import analysis_main
 
-analysis_page = st.Page(analysis_component, title='Analyze Results')
+analysis_page = st.Page(analysis_main, title='Analyze Results')
